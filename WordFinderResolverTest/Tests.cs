900ff71b0cd5bb4e11a93cb33aa59fe748a5c3e1
Juan Gabriel <hartmann_jg@yahoo.com.ar>
2024-06-20 00:00:11 -0300

    add test with random repetitions

--- WordFinderResolverTest/Tests.cs
+++ WordFinderResolverTest/Tests.cs
@@ -33,7 +33,7 @@ namespace WordFinderResolverTest
                     { "A", "B", "C", "A" },
                     { "A", "B", "C", "B"},
                     { "D", "E", "F", "C" },
-                    { "G", "H", "I", "D" },
+                    { "G", "A", "B", "C" },
                     { "O", "A", "D", "G" }
                 }, 
                 Words = new List<string>() { "ADG", "DEF", "CGI", "ABC" }
@@ -105,6 +105,43 @@ namespace WordFinderResolverTest
             Assert.AreEqual("cold", okResult[0]);
             Assert.AreEqual("wind", okResult[1]);
             Assert.AreEqual("chill", okResult[2]);
+        }
+
+        [Test]
+        public void ChallengeTestRandomRepetitions()
+        {
+            // Preparing request
+            var dto = new MatrixColecctionDto()
+            {
+                Matrix = new string[,]
+                {
+                    { "a", "b", "c", "d", "c", "a", "b", "c", "o", "l", "d" },
+                    { "r", "g", "w", "i", "o", "c", "h", "i", "l", "l", "d" },
+                    { "c", "h", "i", "l", "l",  "a", "b", "w", "i", "n", "d"},
+                    { "p", "q", "n", "s", "d" , "w", "i", "n", "d", "d", "d"},
+                    { "u", "v", "d", "x", "y", "c", "h", "x", "l", "l", "d" },
+                    { "a", "b", "c", "d", "c", "c", "h", "d", "l", "l", "d" },
+                    { "r", "g", "w", "i", "o", "c", "h", "i", "l", "l", "d" },
+                    { "c", "h", "i", "l", "l", "d", "c", "h", "i", "l", "l" },
+                    { "p", "q", "n", "s", "d", "c", "o", "l", "d", "d", "g" },
+                    { "u", "v", "d", "x", "y", "c", "h", "i", "l", "l", "g" }
+
+                },
+                Words = new List<string>() { "cold", "wind", "snow", "chill" }
+            };
+
+            // GetWords
+            var result = _controller.Get(dto);
+
+            // Assert
+            Assert.IsInstanceOf<IEnumerable<string>>(result.Result);
+            var okResult = result.Result.ToList();
+            Assert.IsNotNull(okResult);
+
+            Assert.AreEqual("chill", okResult[0]);
+            Assert.AreEqual("cold", okResult[1]);
+            Assert.AreEqual("wind", okResult[2]);
+            
         }
     }
 }
